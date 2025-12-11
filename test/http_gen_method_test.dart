ef9526db5eb291fc0375ffd5aaacdc8b6c801a87
wanjm <wanjm@mac-pro.plaso>
2025-12-11 14:01:24 +0800

    remove error

--- test/http_gen_method_test.dart
+++ test/http_gen_method_test.dart
@@ -1,16 +1,14 @@
-import 'package:http_gen_method/http_gen_method.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('First Test', () async {
+
+      expect("", isNot(null));
     });
   });
 }
